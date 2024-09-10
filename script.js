7946a21fabbea81cb635a19a0732006e4d41358b
Samuel Monteiro <smf.monteiro1701@gmail.com>
2024-09-10 20:51:18 -0300

    commit

--- script.js
+++ script.js
@@ -15,7 +15,30 @@ function startPomodoro (){
     intervalo = setIntervalo(() =>{
         if(tempoRestante >= 0){
             clearInterval(intervalo)
+            alert('o tempo acabpu!')
+            resetPomodoro()
+        }else{
+            tempoRestante--
+            updateDisplay()
         }
-    }
+    }, 1000)
 }
+
+function resetPomodoro(){
+    clearInterval(intervalo)
+    isRunning = false
+    tempoRestante = pomodoroDuration
+    updateDisplay()
+}
+
+function updateDisplay(){
+    let minutes = Math.floor(tempoRestante/60)
+    let seconds = tempoRestante % 60
+
+    minutesDisplay.textContent = minutes.toString().padStart(2, '0')
+    secondsDisplay.textContent = seconds.toString().padStart(2, '0')
+}
+
+startButton.addEventListener('click', startPomodoro)
+startButton.addEventListener('click', resetPomodoro)
 
